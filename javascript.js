78bd54458e29ba9c4acef4501b7547dde46a39fa
tugi-ngunjiri <blueqwinn@gmail.com>
2022-12-05 12:54:02 +0300

    done with assigment

--- javascript.js
+++ javascript.js
@@ -47,6 +47,91 @@ console.log(speedDetector(180));
 
 
 
+//Step 1: Calculate nhif deduction amount
+function nhifCalc(grosspay) {
+  let nhifDeduct;
+  let maxNhifDeduct;
+  let lowerBoundArr=[0, 6000, 8000, 12000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 60000, 70000, 80000, 90000, 100000];
+  let upperBoundArr=[5999, 7999, 11999, 14999, 19999, 24999, 29999, 34999, 39999, 44999, 49999, 59999, 69999, 79999, 89999, 99999];
+  let nhifDeductArr=[150, 300, 400, 500, 600, 750, 850, 900, 950, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700];
+  
+  
+  maxNhifDeduct = nhifDeductArr[nhifDeductArr.length-1]; // for pay above 100,000, max nhif deduction is fixed at 1,700
+
+  for (let i = 0; i < lowerBoundArr.length; i++) {
+      if (i===lowerBoundArr.length-1){
+          return maxNhifDeduct;           
+
+      }else{
+          if (grosspay>=lowerBoundArr[i] & grosspay<=upperBoundArr[i]){
+              return nhifDeductArr[i];
+          }
+      }
+  }
+
+}
+
+//Step 2: Calculate paye before personal relief
+function payeBeforeReliefCalc(grosspay){
+  let taxableIncome;
+  const pension=200;
+  let paye;
+
+  // sub Step 1: Get taxable income
+  taxableIncome = grosspay-pension;
+
+  // sub Step 2: Now calculate paye
+  const taxAmount1=24000*10/100;
+  const taxAmount2=(32333-24000)*25/100;
+
+  if(taxableIncome  > 32333) {
+      paye = taxAmount1 +taxAmount2 + (taxableIncome -32333)*30/100;
+  } else if(taxableIncome  > 24000 && taxableIncome <32333) {
+      paye = taxAmount1 + (taxableIncome -24000)*25/100;
+  } else{
+      paye = (taxableIncome )*10/100;
+  }
+
+  // sub Step 3: Return
+  return paye;
+}
+
+//Step 3: Summation of all deductions
+function deductionsCalc(grosspay){
+  // nssf
+  let nssf=200; //adopting old rates as new rates are still being contested in court
+  console.log('NSSF: '+ nssf)
+  // nhif
+  let nhif=nhifCalc(grosspay);
+  console.log('NHF: ' +nhif);
+
+  // Personal Relief
+  const personalRelief=2400;
+
+  // Paye
+  let payeBeforeRelief = payeBeforeReliefCalc(grosspay);
+  let paye = payeBeforeRelief-(personalRelief);
+
+  console.log('PAYE: ' + paye);
+
+  // Return
+  return nssf+nhif+paye;
+
+}
+
+//Step 4: Calculate Netpay
+function netPayCalc(grosspay){
+
+  // sub Step 1: Get deductions
+  let deductions;
+  deductions = deductionsCalc(grosspay);
+
+  // sub Step 2: Get netpay
+  return grosspay - deductions;
+}
+
+console.log('NETPAY: '+ netPayCalc(80000));
+
 
 
 
